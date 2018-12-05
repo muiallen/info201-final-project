a3387d262c439ae018fc7b32fa47f18bad9b3f4a
muiallen <allencmui@live.com>
2018-12-05 00:18:19 -0800

    Final layout changes to shiny app for better look

--- server.R
+++ server.R
@@ -14,13 +14,16 @@ library(gsubfn)
 # Define server logic required to draw bar graphs of Google Play Store app data
 shinyServer(function(input, output) {
   
-  # Reads Google Play Store data and filters/cleans the the category titles
+  # Reads Google Play Store data and filters/cleans the data for duplicates, correct category titles, and well-formatted install numbers
   app_data <- fread("googleplaystore.csv", stringsAsFactors = FALSE)
   app_data <- subset(app_data, !duplicated(app_data$App))
+  
   app_data$App = str_to_title(app_data$App)
+  
   app_data$Category = str_replace_all(app_data$Category, "_", " ")
   app_data$Category = str_to_title(app_data$Category)
   app_data$Category = str_replace_all(app_data$Category, "And", "&")
+  
   app_data$Installs = gsubfn(".", list("+" = "", "," = ""), app_data$Installs)
   app_data$Installs = as.integer(app_data$Installs)
   
@@ -34,9 +37,11 @@ shinyServer(function(input, output) {
       coord_flip() + theme(legend.position = "none")
   })
   
-  #
+  # Plots dynamic bar graph of top Google Play Store apps at a given price, in a given category, by a specific popularity index 
   output$topAppsBarPlot <- renderPlot({
     graph_title <- "Top"
+    
+    # Filters data by price
     if(input$Price == "All"){
       topApps <- app_data
     } else if(input$Price == "Free"){
@@ -47,11 +52,13 @@ shinyServer(function(input, output) {
       topApps <- filter(app_data, Type == "Paid") 
     }
     
+    # Filters data by category
     if(input$Category != "All") {
       graph_title <- paste0(graph_title, " ", input$Category)
       topApps <- filter(topApps, Category == input$Category)
     }
     
+    # Filters data by installs and graphs top apps
     if(input$Index == "Installs"){
       graph_title <- paste0(graph_title, " Apps by Installs")
       topApps <- arrange(topApps, desc(Installs))
@@ -61,7 +68,8 @@ shinyServer(function(input, output) {
         labs(title = graph_title, x = "App", y = "Number of Installs") + 
         scale_y_continuous(labels = scales::comma) +
         coord_flip() + theme(legend.position = "none")
-      
+    
+    # Filters data by reviews and graphs top apps  
     } else if(input$Index == "Reviews"){
       graph_title <- paste0(graph_title, " Apps by Reviews")
       topApps <- arrange(topApps, desc(Reviews))
@@ -69,24 +77,26 @@ shinyServer(function(input, output) {
 
       ggplot(topApps) + geom_bar(aes(x = reorder(App, Reviews), y = Reviews, fill = App), stat = "identity") +
         labs(title = graph_title, x = "App", y = "Number of Reviews") + 
-        ## scale_y_continuous(labels = scales::comma) +
+        scale_y_continuous(labels = scales::comma) +
         coord_flip() + theme(legend.position = "none")
     }
   })
   
-  # Sets subset based on input category
+  # Creates intro text 
+  #output$intro <- renderText({
+    #paste0("The dataset we will be working with is called Google Play Store Apps. The dataset includes about 
+           #10,000 applications that are on the Google Play Store and detailed information about each application. 
+           #The dataset is from kaggle.com and it has more than 600 upvotes. According to kaggle, Lavanya Gupta, a software 
+           #engineer at HSBC Software Development scraped the data for developer use in "analysing the Android market". 
+           #She found that many of the public datasets on kaggle were about the Apple App store so decided to collect Google Play 
+           #Store data for the public and app-making businesses to use. We have created data visualizations with advertising
+           #companies looking for the right apps to advertise their products on in mind.")
+  #})
   
-  # Creates summary text 
-  output$intro <- renderText({
-    paste0("The dataset we will be working with is called Google Play Store Apps. 
-           The dataset includes about 10,000 applications that are on the Google Play Store 
-           and detailed information about each application. 
-           The dataset is from kaggle.com.")
-  })
-  
+  # Creates summary text
   output$summary <- renderText({
     paste0("There are 33 app categories on the Google Play Store platform. 
-           The top three categories with most apps are Family, GameS, and Tools. 
+           The top three categories with most apps are Family, Game, and Tools. 
            The Family category has ", sum(app_data$Category == "Family"), " apps; Game category has ",
            sum(app_data$Category == "Game"), " apps; Tools category has ", sum(app_data$Category == "Tools"),
            " apps. Advertisement companies tasked with marketing family, game, and tool products 
--- ui.R
+++ ui.R
@@ -1,27 +1,58 @@
 library(shiny)
 
+# Define UI for application that draws graphs of app data in the Google Play Store
 shinyUI(fluidPage(
-  titlePanel("Google Play Store Analysis"),
-  sidebarLayout(
-    sidebarPanel(
-      radioButtons("Price", "Free/Paid Apps", choices = c("All", "Free", "Paid"), selected = "Free"),
-      selectInput("Category", "Choose a Category", choices = c("All", "Art & Design", "Auto & Vehicles", "Beauty", "Books & Reference", 
-                                                               "Business", "Comics", "Communication", "Dating", "Education", 
-                                                               "Entertainment", "Events", "Family", "Finance", "Food & Drink", 
-                                                               "Game", "Health & Fitness", "House & Home", "Libraries & Demo", 
-                                                               "Lifestyle", "Maps & Navigation", "Medical", "News and Magazines", 
-                                                               "Parenting", "Personalization", "Photography", "Productivity", 
-                                                               "Shopping", "Social", "Sports", "Tools", "Travel & Local", 
-                                                               "Videoplayers", "Weather")),
-      radioButtons("Index", 
-                  "Select Index", 
-                  choices = c("Installs", "Reviews"))
-    ),
-    mainPanel(
-      textOutput("intro"),
-      plotOutput("topAppsBarPlot"),
-      plotOutput("totalAppCategoryBarPlot"),
-      textOutput("summary")
+  titlePanel("Google Play Store Analysis (Zikai Song, Allen Mui, Heewon Song)"),
+  
+  mainPanel(
+      tabsetPanel(type = "tabs",
+                  
+                  tabPanel("Introduction",
+                           p("The dataset we will be working with is called Google Play Store Apps. The dataset includes about 
+                              10,000 applications that are on the Google Play Store and detailed information about each application. 
+                              The dataset is from ", a("kaggle.com", href = 'https://www.kaggle.com/lava18/google-play-store-apps'), 
+                              "and it has more than 600 upvotes. According to kaggle, Lavanya Gupta, a software engineer at HSBC 
+                              Software Development scraped the data for developer use in \"analysing the Android market\". She found that 
+                              many of the public datasets on kaggle were about the Apple App store so decided to collect Google Play 
+                              Store data for the public and app-making businesses to use. We have created data visualizations with advertising
+                              companies looking for the right apps to advertise their products on in mind."),
+                           img(src = "google-play.png", height = "75%", width = "75%")
+                           ),
+                  
+                  tabPanel("Popular App Categories", 
+                           plotOutput("totalAppCategoryBarPlot"),
+                           textOutput("summary")
+                           ),
+                  
+                  tabPanel("Top Apps",
+                           # Sidebar with radio and select inputs for price, category, and index for user to create top apps graph
+                           sidebarLayout(sidebarPanel(
+                                           radioButtons("Price", "Free/Paid Apps", choices = c("All", "Free", "Paid"), selected = "Free"),
+                                           
+                                           br(),
+                                           
+                                           selectInput("Category", "Choose a Category", choices = c("All", "Art & Design", "Auto & Vehicles", "Beauty", "Books & Reference", 
+                                                                                                    "Business", "Comics", "Communication", "Dating", "Education", 
+                                                                                                    "Entertainment", "Events", "Family", "Finance", "Food & Drink", 
+                                                                                                    "Game", "Health & Fitness", "House & Home", "Libraries & Demo", 
+                                                                                                    "Lifestyle", "Maps & Navigation", "Medical", "News and Magazines", 
+                                                                                                    "Parenting", "Personalization", "Photography", "Productivity", 
+                                                                                                    "Shopping", "Social", "Sports", "Tools", "Travel & Local", 
+                                                                                                    "Videoplayers", "Weather"), width = "150px"),
+                                           
+                                           br(),
+                                           
+                                           radioButtons("Index", 
+                                                        "Select Index", 
+                                                        choices = c("Installs", "Reviews"))
+                                         ),
+                                         
+                                         mainPanel(
+                                           plotOutput("topAppsBarPlot")
+                                         )
+                           )
+                  )
+      )
     )
   )
-))
+)

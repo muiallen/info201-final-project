77e5514d3e7fe86ecccb5fbefc177dab8fc39fad
muiallen <allencmui@live.com>
2018-12-05 10:19:18 -0800

    Adds final change touches to README for better flow

--- ui.R
+++ ui.R
@@ -8,15 +8,15 @@ shinyUI(fluidPage(
       tabsetPanel(type = "tabs",
                   
                   tabPanel("Introduction",
-                           p("The dataset we will be working with is called Google Play Store Apps. The dataset includes about 
+                           p("The dataset we will be working with is called Google Play Store apps. The dataset includes about 
                               10,000 applications that are on the Google Play Store and detailed information about each application. 
                               The dataset is from ", a("kaggle.com", href = 'https://www.kaggle.com/lava18/google-play-store-apps'), 
                               "and it has more than 600 upvotes. According to kaggle, Lavanya Gupta, a software engineer at HSBC 
                               Software Development scraped the data for developer use in \"analysing the Android market\". She found that 
-                              many of the public datasets on kaggle were about the Apple App store so decided to collect Google Play 
-                              Store data for the public and app-making businesses to use. We have created data visualizations with advertising
-                              companies looking for the right apps to advertise their products on in mind."),
-                           img(src = "google-play.png", height = "75%", width = "75%")
+                              many of the public datasets on kaggle were about the Apple App Store so she decided to collect Google Play 
+                              Store data for the public and app-making businesses to use. With Shiny, we have created data 
+                              visualizations with advertising companies looking for the right apps to advertise their products on in mind."),
+                           img(src = "google-play.png", height = "70%", width = "70%")
                            ),
                   
                   tabPanel("Popular App Categories", 
